21997277e815215b4e809b0f7f2ff7b5f1110741
tjacobhi <tjhill_94@hotmail.com>
2016-12-29 13:50:44 -0700

    Having basic graphics control in "TestWindow"

--- src/net/tjacobhi/samebird/TestWindow.java
+++ src/net/tjacobhi/samebird/TestWindow.java
@@ -18,10 +18,11 @@ public class TestWindow extends JPanel{
         private static void createAndShowGUI() {
             //Create and set up the window.
             JFrame frame = new LineJFrame(); //notice this line was changed to my own created inner class
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
             JLabel emptyLabel = new JLabel("");
-            //here I was experimenting trying to force window size
+
+            //here I was experimenting trying to force window size, doesn't quite work yet, but is getting there
             emptyLabel.setPreferredSize(new Dimension(800, 600));
             emptyLabel.setMinimumSize(new Dimension(800, 600));
             emptyLabel.setMaximumSize(new Dimension(800, 600));
@@ -57,9 +58,11 @@ public class TestWindow extends JPanel{
             {
                 super.paint(g); // I added this line to handle repaint issues
                 g.setColor(new Color(0x4488ee)); // This allows us to create a color using hex value
-                g.fillRect(0, 0, 800, 600); // Draws a filled in rectangle
+                g.fillRect(60, 60, 80, 120); // Draws a filled in rectangle
                 g.setColor(Color.red); // This allows us to use color name (very limited)
                 g.drawLine(50, 200, 400, 300); // Draws a line
+                g.setColor(new Color(0x21e249));
+                g.drawString("Hello, World!", 380, 290); // Draws text
             }
         }
 
