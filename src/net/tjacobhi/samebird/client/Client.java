42c8e9dec9fb1a21890cb64ab92c667360bcc384
tjacobhi <tjhill_94@hotmail.com>
2016-12-29 14:36:02 -0700

    Moved Client into its own package

--- src/net/tjacobhi/samebird/client/Client.java
+++ src/net/tjacobhi/samebird/client/Client.java
@@ -2,8 +2,11 @@ package net.tjacobhi.samebird.client;
 
 import net.tjacobhi.samebird.TestWindow;
 
-import java.awt.*;
-
+/**
+ * Created by tjacobhi on 29 December 2016
+ *
+ * Client is the starting point for the client-end of same-bird
+ */
 public class Client
 {
     TestWindow mWindow; // I'm creating its window as a member variable.
@@ -13,6 +16,10 @@ public class Client
         mWindow = new TestWindow();
     }
 
+    /**
+     * This is the entry point of the Client
+     * @param args any command args may be placed here (We can handle them to do specific things)
+     */
     public static void main(String[] args)
     {
         Client client = new Client();
