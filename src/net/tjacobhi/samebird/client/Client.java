cc991a43c1784e685f0aeed4d7ba3f814b4e9f5b
TimePastry <timepastry@gmail.com>
2016-12-30 15:50:37 -0700

    All three serverapplication threads are working

--- src/net/tjacobhi/samebird/client/Client.java
+++ src/net/tjacobhi/samebird/client/Client.java
@@ -39,26 +39,13 @@ public class Client
 				BufferedReader in = new BufferedReader(
 						new InputStreamReader(socket.getInputStream()));
 		) {
-			BufferedReader stdIn =
-					new BufferedReader(new InputStreamReader(System.in));
-			String fromServer;
-			String fromUser;
-		
-			while ((fromServer = in.readLine()) != null) {
-				System.out.println("Server: " + fromServer);
-				if (fromServer.equals("Bye."))
-					break;
-			
-				fromUser = stdIn.readLine();
-				if (fromUser != null) {
-					System.out.println("Client: " + fromUser);
-					out.println(fromUser);
-				}
-			}
-		} catch (UnknownHostException e) {
+			out.println("I've connected!");
+		}
+		catch (UnknownHostException e) {
 			System.err.println("Don't know about host ");
 			System.exit(1);
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.err.println("Couldn't get I/O for the connection to ");
 			System.exit(1);
 		}
