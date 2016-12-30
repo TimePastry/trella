0acd008f7b4b7ae24edb0ce3f5dd66ab115717fb
TimePastry <timepastry@gmail.com>
2016-12-30 15:35:36 -0700

    Basic server operation, echoes a server connection

---
+++ src/net/tjacobhi/samebird/Utilities/Utilites.java
@@ -0,0 +1,10 @@
+package net.tjacobhi.samebird.Utilities;
+
+/**
+ * Created by Sean on 12/30/2016.
+ *
+ * contains utility constants
+ */
+public class Utilites {
+	public static final int PORT = 25560;
+}
--- src/net/tjacobhi/samebird/server/Server.java
+++ src/net/tjacobhi/samebird/server/Server.java
@@ -1,15 +1,42 @@
 package net.tjacobhi.samebird.server;
 
+import net.tjacobhi.samebird.Utilities.Utilites;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Sean on 12/30/2016.
  *
  * connects to, receives information from, and sends information to the clients
  */
 public class Server implements Runnable{
-	
+	private Thread mThread;
 	
 	@Override
 	public void run() {
-		
+		try(
+				ServerSocket serversocket = new ServerSocket(Utilites.PORT);
+				Socket clientsocket = serversocket.accept();
+				PrintWriter out = new PrintWriter(clientsocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientsocket.getInputStream()));
+				)
+		{
+			System.out.println(in.readLine());
+		}catch (IOException e){
+			System.out.println("IOException! Stop breaking things!");
+		}
+				
+	}
+	
+	void start(){
+		if (mThread == null){
+			mThread = new Thread(this, "Console");
+			mThread.start();
+		}
 	}
 }
