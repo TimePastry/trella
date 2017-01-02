85ced949671b32e19ad7587241e4661041ec10f4
tjacobhi <tjhill_94@hotmail.com>
2017-01-02 16:17:14 -0700

    Created the empty class for the create user screen

---
+++ src/net/tjacobhi/samebird/client/client_pages/CreateUserScreen.java
@@ -0,0 +1,17 @@
+package net.tjacobhi.samebird.client.client_pages;
+
+import javax.swing.*;
+
+/**
+ * Created by tjacobhi on 02-Jan-17.
+ */
+public class CreateUserScreen extends JPanel implements Reloadable
+{
+
+
+	@Override
+	public void reload()
+	{
+
+	}
+}
